b6eb7e1870ac688e042a47d8b4961cd91ed58337
mrrobot242 <gclopes132@gmail.com>
2023-07-13 01:58:10 -0300

    supabase setup

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,5 +1,3 @@
-
-
 import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
 
@@ -27,10 +25,20 @@ export default function Home() {
           gap-3
           mt-4
           ">
-            <ListItem />
+            <ListItem
+              image="/images/liked.png"
+              name="Liked Songs"
+              href="liked"
+            />
           </div>
         </div>
       </Header>
+      <div className="mt-2 mb-7 px-6">
+        <div className="flex justify-between items-center">
+          <h1 className="text-white text-2xl font-semibold">Newest Songs</h1>
+        </div>
+        <div>List of Songs</div>
+      </div>
     </div>
   );
 }
