56845203630f4fd1e5851ae258592f420df9c5b6
GustavoGuerato <guerato.gustavo14@gmail.com>
2023-12-13 20:10:20 -0300

    some bugs are fixed

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,16 +1,22 @@
-import React from "react";
 import { forwardRef } from "react";
 import { twMerge } from "tailwind-merge";
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
-const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, children, disabled, type = "button", ...props }, ref) => {
-    return (
-      <button
-        type={type}
-        className={twMerge(
-          `
-             w-full 
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
+  className,
+  children,
+  disabled,
+  type = 'button',
+  ...props
+}, ref) => {
+  return (
+    <button
+      type={type}
+      className={twMerge(
+        `
+        w-full 
         rounded-full 
         bg-green-500
         border
@@ -22,17 +28,20 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
         text-black
         font-bold
         hover:opacity-75
-        transition`,
-          className
-        )}
-        disabled={disabled}
-        ref={ref}
-        {...props}>
-        {children}
-      </button>
-    );
-  }
-);
+        transition
+      `,
+        disabled && 'opacity-75 cursor-not-allowed',
+        className
+      )}
+      disabled={disabled}
+      ref={ref}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+});
 
 Button.displayName = "Button";
+
 export default Button;
--- components/SidebarItem.tsx
+++ components/SidebarItem.tsx
@@ -1,7 +1,7 @@
-import Link from "next/link";
-import React from "react";
-import { IconType } from "react-icons";
-import { twMerge } from "tailwind-merge";
+import Link from 'next/link';
+import { IconType } from 'react-icons';
+import { twMerge } from 'tailwind-merge';
+
 interface SidebarItemProps {
   icon: IconType;
   label: string;
@@ -13,13 +13,12 @@ const SidebarItem: React.FC<SidebarItemProps> = ({
   icon: Icon,
   label,
   active,
-  href,
+  href
 }) => {
-  return (
+  return ( 
     <Link
-      href={href}
-      className={twMerge(
-        `
+      href={href} 
+      className={twMerge(`
         flex 
         flex-row 
         h-auto 
@@ -27,18 +26,20 @@ const SidebarItem: React.FC<SidebarItemProps> = ({
         w-full 
         gap-x-4 
         text-md 
-        font-medium 
+        font-medium
         cursor-pointer
-      hover:text-white 
+        hover:text-white
         transition
         text-neutral-400
         py-1`,
         active && "text-white"
-      )}>
+        )
+      }
+    >
       <Icon size={26} />
       <p className="truncate w-100">{label}</p>
     </Link>
-  );
-};
+   );
+}
 
 export default SidebarItem;
